b831666f95a10ec7654a219c7915f6d1f3fa38d0
tony <lucy4116411@gmail.com>
2020-06-19 22:38:39 +0800

    Initial Commit

---
+++ ttt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ttt
+//
+//  Created by User01 on 2020/6/19.
+//  Copyright © 2020 SW. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
